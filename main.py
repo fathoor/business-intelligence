a11387f10aa039ee50e53bf89d68f10755e7ea45
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-09-30 13:02:40 +0700

    fix: resolved (#1)
    removed `from load import *` as it appears to run the whole program, even before the `exists` validation start

--- main.py
+++ main.py
@@ -2,19 +2,19 @@
 
 import os
 from os.path import exists
-from load import *
 
 print("# Kecerdasan Bisnis [C]\n")
 print("ETL dengan Python menggunakan library pETL\n")
-print("Muhammad Fathurrahman - 5026201139\n")
-print("Sedang melakukan ETL process...\n")
+print("Muhammad Fathurrahman - 5026201139\n\n")
+print("Sedang melakukan ETL...\n")
 
 # ETL
 if exists("pelanggan_transform.csv"):
-    print("Menghapus file pelanggan_transform.csv yang sudah ada...\n\n")
+    print("Menghapus file pelanggan_transform.csv yang sudah ada...\n")
     os.remove("pelanggan_transform.csv")
+    print("File pelanggan_transform.csv berhasil dihapus\n\n")
     os.system("python load.py")
 else :
     os.system("python load.py")
 
-print("ETL process selesai\n\n")
+print("ETL selesai\n\n")
